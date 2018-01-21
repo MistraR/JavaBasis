750fce30e200eebeeeedc4c7eee31bf6abf35f38
Mistra <842404548@qq.com>
2018-01-21 20:00:10 +0800

    工厂方法模式测试

---
+++ src/mistra/com/designpatterns/Factory/Factory1.java
@@ -0,0 +1,18 @@
+package mistra.com.designpatterns.Factory;
+
+/**
+ * Created by Mistra-WR on 2018/1/21/021.
+ */
+public class Factory1 extends HandOverCarAbstract{
+    @Override
+    public Car SalesManagerGetCar(String type) {
+        Car car = null;
+        switch (type){
+            case "RS3": car = new AudiRS3();
+            case "RS5": car = new AudiRS5();
+            case "RS7": car = new AudiRS7();
+            default:;
+        }
+        return car;
+    }
+}
--- src/mistra/com/designpatterns/Factory/HandOverCar.java
+++ src/mistra/com/designpatterns/Factory/HandOverCar.java
@@ -3,10 +3,6 @@ package mistra.com.designpatterns.Factory;
 /**
  * Created by Mistra-WR on 2018/1/18/018.
  */
-
-/**
- * 从仓库得到汽车
- */
 public class HandOverCar {
     private SSSSFactroy sf;
     public HandOverCar(SSSSFactroy sf){
