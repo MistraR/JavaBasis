22ae78aa73c1b750c67126934e244f622d1b5b50
Mistra <842404548@qq.com>
2018-01-28 14:28:42 +0800

    冒泡排序测试

--- src/mistra/com/util/sorting/Test.java
+++ src/mistra/com/util/sorting/Test.java
@@ -12,6 +12,7 @@ public class Test {
         int [] a ={1,56,48,889,75,356,2};
         int temp = 0;
         for(int i=0;i<a.length-1;i++){
+
             for (int j=0;j<a.length-1-i;j++){
                 if(a[j]>a[j+1]){
                     temp=a[j];
