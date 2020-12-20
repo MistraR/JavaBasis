db80b9078a9afadf02607ef25c238479b95a9b43
MistraR <842404548@qq.com>
2020-12-20 14:28:13 +0800

    正确停止线程

---
+++ src/mistra/com/threadcore/stopthread/RightWayStopThreadWithoutSleep.java
@@ -0,0 +1,32 @@
+package mistra.com.threadcore.stopthread;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/20 下午2:27
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class RightWayStopThreadWithoutSleep implements Runnable{
+
+    @Override
+    public void run() {
+        int num = 0;
+        while (!Thread.currentThread().isInterrupted() && num <= Integer.MAX_VALUE / 2) {
+            if (num % 10000 == 0) {
+                System.out.println(num + "是10000的倍数");
+            }
+            num++;
+        }
+        System.out.println("任务运行结束了");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new RightWayStopThreadWithoutSleep());
+        thread.start();
+        Thread.sleep(2000);
+        thread.interrupt();
+    }
+}
