f827fef076d2d98ba107087268fa0bb067e72a5e
MistraR <842404548@qq.com>
2020-12-21 22:09:39 +0800

    演示用volatile的局限

---
+++ src/mistra/com/threadcore/stopthread/volatiledemo/WrongWayVolatile.java
@@ -0,0 +1,39 @@
+package mistra.com.threadcore.stopthread.volatiledemo;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/20 下午2:36
+ * @ Description: 演示用volatile的局限：part1 看似可行
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class WrongWayVolatile implements Runnable{
+
+    private volatile boolean canceled = false;
+
+    @Override
+    public void run() {
+        int num = 0;
+        try {
+            while (num <= 100000 && !canceled) {
+                if (num % 100 == 0) {
+                    System.out.println(num + "是100的倍数。");
+                }
+                num++;
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        WrongWayVolatile r = new WrongWayVolatile();
+        Thread thread = new Thread(r);
+        thread.start();
+        Thread.sleep(5000);
+        r.canceled = true;
+    }
+}
