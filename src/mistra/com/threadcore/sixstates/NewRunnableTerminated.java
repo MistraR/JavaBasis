bf357eba99a1a989e2fdce572136cd4b9d21c8bd
MistraR <842404548@qq.com>
2020-12-26 13:33:31 +0800

    NewRunnableTerminated

---
+++ src/mistra/com/threadcore/sixstates/NewRunnableTerminated.java
@@ -0,0 +1,42 @@
+package mistra.com.threadcore.sixstates;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/20 下午2:39
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class NewRunnableTerminated implements Runnable{
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new NewRunnableTerminated());
+        //打印出NEW的状态
+        System.out.println(thread.getState());
+        thread.start();
+        System.out.println(thread.getState());
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //打印出RUNNABLE的状态，即使是正在运行，也是RUNNABLE，而不是RUNNING
+        System.out.println(thread.getState());
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //打印出TERMINATED状态
+        System.out.println(thread.getState());
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
