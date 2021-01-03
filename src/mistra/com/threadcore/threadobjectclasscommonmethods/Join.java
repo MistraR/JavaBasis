c67e829ce4c0a9f6a02746324a8dc9a7914190e6
MistraR <842404548@qq.com>
2021-01-03 14:52:43 +0800

    join

---
+++ src/mistra/com/threadcore/threadobjectclasscommonmethods/Join.java
@@ -0,0 +1,44 @@
+package mistra.com.threadcore.threadobjectclasscommonmethods;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/26 下午3:24
+ * @ Description: 演示join，注意语句输出顺序，会变化。
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class Join {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "执行完毕");
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "执行完毕");
+            }
+        });
+
+        thread.start();
+        thread2.start();
+        System.out.println("开始等待子线程运行完毕");
+        thread.join();
+        thread2.join();
+        System.out.println("所有子线程执行完毕");
+    }
+}
