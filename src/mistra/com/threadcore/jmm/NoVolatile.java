ce124dcc858030684b83cc255ce4884688d6864e
MistraR <842404548@qq.com>
2021-02-06 12:57:38 +0800

    NoVolatile

---
+++ src/mistra/com/threadcore/jmm/NoVolatile.java
@@ -0,0 +1,37 @@
+package mistra.com.threadcore.jmm;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/27 下午2:51
+ * @ Description: 不适用于volatile的场景
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class NoVolatile implements Runnable{
+
+    volatile int a;
+    AtomicInteger realA = new AtomicInteger();
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable r =  new NoVolatile();
+        Thread thread1 = new Thread(r);
+        Thread thread2 = new Thread(r);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(((NoVolatile) r).a);
+        System.out.println(((NoVolatile) r).realA.get());
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            a++;
+            realA.incrementAndGet();
+        }
+    }
+}
