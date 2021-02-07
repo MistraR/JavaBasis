5c649fb297ea3edffee488f77f571aea8b905419
MistraR <842404548@qq.com>
2021-02-07 16:46:04 +0800

    FieldVisibility

---
+++ src/mistra/com/threadcore/jmm/FieldVisibility.java
@@ -0,0 +1,56 @@
+package mistra.com.threadcore.jmm;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/27 下午1:54
+ * @ Description: 演示可见性带来的问题
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class FieldVisibility {
+
+    volatile int a = 1;
+    volatile int b = 2;
+
+    private void change() {
+        a = 3;
+        b = a;
+    }
+
+
+    private void print() {
+        System.out.println("b=" + b + ";a=" + a);
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            FieldVisibility test = new FieldVisibility();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    test.change();
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    test.print();
+                }
+            }).start();
+        }
+
+    }
+}
