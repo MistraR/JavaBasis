f4136c32f0223d78ae7676fc497b9a5331e97e3d
MistraR <842404548@qq.com>
2021-02-20 16:15:31 +0800

    MustDeadLock

---
+++ src/mistra/com/threadcore/deadlock/MustDeadLock.java
@@ -0,0 +1,57 @@
+package mistra.com.threadcore.deadlock;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/12/27 下午7:29
+ * @ Description: 必定发生死锁的情况
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class MustDeadLock implements Runnable{
+    int flag = 1;
+
+    static Object o1 = new Object();
+    static Object o2 = new Object();
+
+    public static void main(String[] args) {
+        MustDeadLock r1 = new MustDeadLock();
+        MustDeadLock r2 = new MustDeadLock();
+        r1.flag = 1;
+        r2.flag = 0;
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r2);
+        t1.start();
+        t2.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("flag = " + flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o2) {
+                    System.out.println("线程1成功拿到两把锁");
+                }
+            }
+        }
+        if (flag == 0) {
+            synchronized (o2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o1) {
+                    System.out.println("线程2成功拿到两把锁");
+                }
+            }
+        }
+    }
+}
