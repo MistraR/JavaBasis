d43b627ea29be627af20824414cde00d477c291c
MistraR <842404548@qq.com>
2021-03-28 10:30:46 +0800

    双重校验锁单例

--- src/mistra/com/designpatterns/singleton/Singleton.java
+++ src/mistra/com/designpatterns/singleton/Singleton.java
@@ -4,9 +4,20 @@ package mistra.com.designpatterns.singleton;
  * Created by Mistra-WR on 2018/1/18/018.
  */
 public class Singleton {
-    private static Singleton instance=new Singleton();
-    private Singleton(){};
-    public static Singleton getInstance(){
+
+    private static volatile Singleton instance = null;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
         return instance;
     }
 }
